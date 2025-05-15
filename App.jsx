bafd1df188b2f2bd6aa815252d668fe1e19b3b2a
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-15 19:59:15 +0545

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+<BrowserRouter>
+     
+      
+      <Routes>
+        
+        
+
+        
+        <Route path='/' element = {<MainLayout/>}>
+        <Route index element={<HomePage />} />
+        </Route>
+        
+
+        
+        
+     
+        
+      </Routes>
+
+       
+    </BrowserRouter>
