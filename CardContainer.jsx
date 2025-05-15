59732e88c64fcd1d050e7113e77b497defc7f47d
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-15 18:33:12 +0545

    Create CardContainer.jsx

---
+++ CardContainer.jsx
@@ -0,0 +1,23 @@
+<section className="py-5" id='shop'>
+      <h4 style={{textAlign:"center"}}>Our Products</h4>
+
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row justify-content-center">
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          
+
+        </div>
+
+
+      </div>
+
+
+
+    </section>
