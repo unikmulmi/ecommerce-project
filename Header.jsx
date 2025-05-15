467ae24d19797bae06d30be17e7d0f807123f41e
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-15 14:31:24 +0545

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+<header className="py-5" style={{backgroundColor: "#6050DC"}}>
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+                <h1 className="display-4 fw-bold">Welcome To Your Favourite Store</h1>
+                <p className="lead fw-normal text-white mb-4">Discover the latest trends with our Store</p>
+                <a href="#" className="btn btn-light btn-lg rounded-pill px-4 py-2">Shop Now</a>
+
+            </div>
+
+        </div>
+
+
+    </header>
