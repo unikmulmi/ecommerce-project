77c0e871aab5478b986c7ac653e5aa456be974a3
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-15 14:22:29 +0545

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,23 @@
+ <footer className = "py-3" style = {{backgroundColor: '#6050DC' , color: 'white'}}>
+    <div className="container text-center">
+     {/* Quick Links Section */}
+      <div className="mb-2">
+        <a href='#' className="text-white text-decoration-none mx-2">Home</a>
+        <a href='#' className="text-white text-decoration-none mx-2">About</a>
+        <a href='#' className="text-white text-decoration-none mx-2">Shop</a>
+        <a href='#' className="text-white text-decoration-none mx-2">Contact</a>
+      </div>
+
+      {/* Social Media Icons Section */}
+      <div className="mb-2">
+      <a href="#" className="text-white mx-2"><FaFacebook/></a>
+      <a href="#" className="text-white mx-2"><FaTwitter/></a>
+      <a href="#" className="text-white mx-2"><FaInstagram/></a>
+      </div>
+       
+       {/* CopyRight Section */}
+
+       <p className='small mb-0'>&copy; 2025 ShopHeX</p>
+      </div>
+
+    </footer>
