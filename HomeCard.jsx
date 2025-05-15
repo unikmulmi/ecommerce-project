a48b5b95bbbf7d81fa4593b26f38a737c4069a9b
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-15 18:55:15 +0545

    HomeCard.jsx

---
+++ HomeCard.jsx
@@ -0,0 +1,19 @@
+<div className={`col-md-3 ${styles.cardWrapper}`}>
+    <Link to="/detail" className={styles.link}>
+    <div className={styles.card}>
+        <div className={styles.cardImgWrapper}>
+            <img src=''
+                className={styles.cardImgTop}
+                alt='Product-Image'/>
+        </div>
+
+    <div className={styles.cardBody}>
+        <h5 className={`${styles.cardTitle} mb-1`}>Product Name</h5>
+        <h6 className={styles.cardText}>$100.00</h6>
+    </div>    
+
+    </div>
+    </Link>
+
+
+    </div>
