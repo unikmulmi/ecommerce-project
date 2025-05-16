8e03f4475c5aead6b70dad49bebd49e387c61b48
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-16 14:25:09 +0545

    Update NotFoundPage.jsx

--- NotFoundPage.jsx
+++ NotFoundPage.jsx
@@ -3,7 +3,7 @@
             <div className="text-center text-white">
                 <h1 className="display-4 few-bold">Page Not Found</h1>
                 <p className="lead fw-normal text-white-75 mb-4">The page you tried accessing does not exist</p>
-                <Link href="#shop" className="btn btn-light btn-lg rounded-pill px-4 py-2">Back Home</Link>
+                <Link to="/" className="btn btn-light btn-lg rounded-pill px-4 py-2">Back Home</Link>
             </div>
         </div>
     </header>
