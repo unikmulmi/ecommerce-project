2913106014b788feca9b2ffda630b5cd6f4b6729
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-16 13:54:35 +0545

    Create NotFoundPage.jsx

---
+++ NotFoundPage.jsx
@@ -0,0 +1,9 @@
+<header className="py-3 my-5" style={{backgroundColor: '#6050DC'}}>
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+                <h1 className="display-4 few-bold">Page Not Found</h1>
+                <p className="lead fw-normal text-white-75 mb-4">The page you tried accessing does not exist</p>
+                <Link href="#shop" className="btn btn-light btn-lg rounded-pill px-4 py-2">Back Home</Link>
+            </div>
+        </div>
+    </header>
