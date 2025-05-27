0b766129d3893787a1d5ffd75919e6e1b37ec406
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-05-27 14:54:32 +0545

    Create RelatedProducts.jsx

---
+++ RelatedProducts.jsx
@@ -0,0 +1,9 @@
+    <section className="py-3 bg-light">
+        <div className="container px-4 px-lg-5 mt-3">
+            <h2 className="fw-bolder mb-4">Related Products</h2>
+            <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                {/* <HomeCard /> */}
+            </div>
+        </div>
+    </section>
+    
