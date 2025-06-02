d3cfdba128ec62fbb2b2108c4647821073dedcca
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-06-02 14:46:02 +0545

    Create GenerateCartCode.js

---
+++ GenerateCartCode.js
@@ -0,0 +1,13 @@
+function generateRandomAlphaNumericValue(length = 10) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    
+    return result;
+}
+
+export const randomValue = generateRandomAlphaNumericValue();
