64ee3daf6ca577ab8d3cc2ecc78b340a8edbff93
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-06-09 15:53:16 +0545

    Create CartPage.jsx

---
+++ CartPage.jsx
@@ -0,0 +1,13 @@
+    <div className="container my-3 py-3" style={{height:'80vh',overflow:'scroll'}}>
+        <h5 className="mb-4">Shopping Cart</h5>
+        <div className="row">
+            <div className="col-md-8">
+                <CartItem />
+                <CartItem />
+                <CartItem />
+
+            </div>
+            <CartSummary />
+        </div>
+        
+    </div>
