42645aef26a324cab34b4843802fde26c3b91c4a
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-06-23 11:43:23 +0545

    Create OrderSummary.jsx

---
+++ OrderSummary.jsx
@@ -0,0 +1,26 @@
+import styles from './OrderSummary.module.css';
+
+    <div className="col-md-8">
+        <div className={`card mb-4 ${styles.card} `}>
+            <div className="card-header" style={{backgroundColor : '#6050DC' , color:"white"}}>
+            <h5>Cart Summary</h5>
+            </div>
+            <div className="card-body">
+                <div className="px-3" style={{height:"300px" , overflow:"auto"}}>
+
+                {/* <OrderItem /> */}
+                    
+
+
+                </div>
+                <hr />
+                <div className="d-flex justify-content-between">
+                    <h6>Total</h6>
+                    <h6>TOTAL PRICE</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+  
+
+
