e7488dd5eb39367af9227bf3efe2dc4f5b4b7d6a
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-06-23 11:40:54 +0545

    Create OrderItem.jsx

---
+++ OrderItem.jsx
@@ -0,0 +1,21 @@
+
+    <div className="d-flex justify-content-between align-items-center mb-3" style={{padding: "10px 0"}}>
+    <div className="d-flex align-items-center">
+
+        <img 
+        src=``
+        alt='Product'
+        className='img-fluid'
+        style={{width : '60px' , height: "60px" , objectFit:"cover" , borderRadius:"5px"}}
+        />
+        <div className="ms-3">
+            <h6 className="mb-0">Product Name}</h6>
+            <small>Quantity : 1</small>
+
+        </div>
+    </div>
+    <h6>price</h6>
+
+    </div>
+  
+
