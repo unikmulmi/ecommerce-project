b83e3aa13b10407078a5fd7c585253c129f49a00
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-07-15 12:44:00 +0545

    Update NavBarLink.jsx

--- NavBarLink.jsx
+++ NavBarLink.jsx
@@ -1,15 +1,15 @@
-<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className='nav-item'>
-            <a className='nav-link active fw-semibold' href='#'>Home</a>
-        </li>
-        <li className='nav-item'>
-            <Link to="/profile" className='nav-link fw-semibold' href="#">Shop</Link>
-        </li>
-        <li className='nav-item'>
-            <a className='nav-link active fw-semibold' href='#'>About</a>
-        </li>
-        <li className='nav-item'>
-            <a className='nav-link active fw-semibold' href='#'>Contact</a>
-        </li>
 
-    </ul>
+<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+  <li className='nav-item'>
+    <Link to="/" className='nav-link fw-semibold'>Home</Link>
+  </li>
+  <li className='nav-item'>
+    <Link to="/shop" className='nav-link fw-semibold'>Shop</Link>
+  </li>
+  <li className='nav-item'>
+    <Link to="/about" className='nav-link fw-semibold'>About</Link>
+  </li>
+  <li className='nav-item'>
+    <Link to="/contact" className='nav-link fw-semibold'>Contact</Link>
+  </li>
+</ul>
