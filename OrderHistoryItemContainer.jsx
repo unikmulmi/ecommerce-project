4ce3ecf05b7cbbf4dd15e3ab4ee9822efced599b
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-07-30 17:05:23 +0545

    Create OrderHistoryItemContainer.jsx

---
+++ OrderHistoryItemContainer.jsx
@@ -0,0 +1,15 @@
+    <div className="row" style={{height:"300px" , overflow:"auto"}}>
+        <div className="col-md-12">
+            <div className="card">
+                <div className="card-header" style={{backgroundColor: '#784dac' , color:'white'}}>
+                    <h5>Order History</h5>
+                </div>
+
+                <OrderHistoryItem/>
+                <OrderHistoryItem/>
+                <OrderHistoryItem/>
+
+
+            </div>
+        </div>
+    </div>
