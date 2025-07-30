ca7caf87723401df3e8f7935ddb1052589b7225a
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-07-30 16:50:26 +0545

    Create UserInfo.jsx

---
+++ UserInfo.jsx
@@ -0,0 +1,44 @@
+    <div className="row mb-4">
+    <div className={`col-md-3 py-3 card ${StyleSheet.textCenter}`}>
+        <img 
+        src={pic}
+        alt='User Profile'
+        className={`img-fluid rounded circle mb-3 mx-auto ${StyleSheet.profileImage}`}
+        />
+        <h4>Unik Mulmi</h4>
+        <p className='text-muted'>Unik.Mulmi@example.com</p>
+        <button className="btn mt-2" style={{backgroundColor: '#784dac' , color:'white'}}></button>
+    </div>
+    <div className="col-md-9">
+        <div className="card">
+            <div className="card-header" style={{backgroundColor: '#784dac' , color: 'white'}}></div>
+            <h5>Account Overview</h5>
+        </div>
+        <div className="card-body">
+            <div className="row">
+                <div className="col-md-6">
+                   <p>
+                    <strong>Full Name:</strong> Unik Mulmi
+                   </p> 
+                   <p>
+                    <strong>Email:</strong> Unik.Mulmi@example.com
+                   </p> 
+                   <p>
+                    <strong>Phone:</strong> +977 9813823568
+                   </p> 
+                   <div className="col-md-6">
+                    <p>
+                    <strong>City:</strong> Dhobighat
+                   </p> 
+                   <p>
+                    <strong>Country</strong> Nepal
+                   </p> 
+                   <p>
+                    <strong>Member Since:</strong> Janaury 2021
+                   </p> 
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
