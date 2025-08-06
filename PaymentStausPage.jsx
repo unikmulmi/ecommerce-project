2204824ef0eeeb747edaf12e7b7e8effbcc46071
unikmulmi <95853684+unikmulmi@users.noreply.github.com>
2025-08-06 15:45:35 +0545

    Create PaymentStausPage.jsx

---
+++ PaymentStausPage.jsx
@@ -0,0 +1,12 @@
+    <header className="py-5" style={{backgroundColor: "#784dac"}}>
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+                <h2 className="display-4 fw-bold">Verifying Payment!</h2>
+                <p className="lead fw-normal text-white-75 mb-4">Give us a moment we are verifying your payment! .</p>
+                <span>
+                    <Link to="#shop" className="btn btn-light btn-lg px-4 py-2 mx-3">View Order Details</Link>
+                    <Link to="#shop" className="btn btn-light btn-lg px-4 py-2">Continue Shopping</Link>
+                </span>
+            </div>
+        </div>
+    </header>
